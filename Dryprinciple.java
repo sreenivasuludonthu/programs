5b7511d2d165c2f0f67759e94592dcea7ee73133
sreenivasulu <60573306+sreenivasuludonthu@users.noreply.github.com>
2020-02-16 20:32:40 +0530

    Add files via upload

---
+++ Dryprinciple.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Dryprinciple
+{
+	public static void main(String[] args) {
+	    double c;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the vaues");
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Division");
+        int ch=s.nextInt();
+        switch(ch)
+        {
+            case 1:
+                c = a+b;
+                break;
+            case 2:
+                c = a-b;
+                break;
+            case 3:
+                c = a*b;
+                break;
+            case 4:
+                c= a /b;
+                break;
+            default:
+                System.out.printf("Enter proper option");
+                return;
+        }
+        System.out.printf("Result :"+c);
+	}
+}
